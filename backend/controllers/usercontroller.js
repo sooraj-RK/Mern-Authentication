0158cd615b34a1fc69c8e9615cf98586e2551ed9
Sooraj Rajkumar <iamsoorajrajkumar@gmail.com>
2024-04-22 00:19:53 +0530

    backend api completed

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -9,13 +9,13 @@ const authUser = asyncHandler(async (req, res) => {
 
   if(user && (await user.matchPassword(password))){
     generateToken(res,user._id);
-    res.status(201).json({
+    res.json({
       _id:user._id,
       name: user.name,
       email: user.email,
     });
   }else{
-    res.status(400);
+    res.status(401);
     throw new Error('Invalid email or password');
   }
 });
@@ -61,11 +61,35 @@ const logoutUser = asyncHandler(async (req, res) => {
   });
 
   const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: " User profile" });
+    const user ={
+      _id: req.user._id,
+      name: req.user.name,
+      email: req.user.email
+    };
+    res.status(200).json(user)
   });
   
   const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Update User profile" });
+    const user = await User.findById(req.user._id);
+
+    if(user){
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+
+      if(req.body.password){
+        user.password = req.body.password;
+      }
+      const updatedUser = await user.save();
+
+      res.status(200).json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+      })
+    }else{
+      res.status(404);
+      throw new Error('user not found')
+    }
   });
 
 export { authUser,
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) =>{
 
 res.cookie('jwt',token,{
     httpOnly: true,
-    secure:process.env.NODe_ENV !== 'developement',
+    secure:process.env.NODE_ENV !== 'developement',
     sameSite: 'strict',
     maxAge: 30 * 24 * 60 *1000
 })
