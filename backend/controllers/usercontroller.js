bc3a54d0a7ff50edc7bca659b0b4c8ce50bc65d1
Sooraj Rajkumar <iamsoorajrajkumar@gmail.com>
2024-05-06 09:13:51 +0530

    frontend start

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -2,23 +2,7 @@ import asyncHandler from "express-async-handler";
 import User from '../models/userModel.js'
 import generateToken from "../utils/generateToken.js";
 
-const authUser = asyncHandler(async (req, res) => {
-  const {email,password} = req.body;
 
-  const user = await User.findOne({email});
-
-  if(user && (await user.matchPassword(password))){
-    generateToken(res,user._id);
-    res.json({
-      _id:user._id,
-      name: user.name,
-      email: user.email,
-    });
-  }else{
-    res.status(401);
-    throw new Error('Invalid email or password');
-  }
-});
 
 const registerUser = asyncHandler(async (req, res) => {
   const {name,email,password} = req.body;
@@ -48,6 +32,24 @@ const registerUser = asyncHandler(async (req, res) => {
   }else{
     res.status(400);
     throw new Error('Invalid user data')
+  }
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  const {email,password} = req.body;
+
+  const user = await User.findOne({email});
+
+  if(user && (await user.matchPassword(password))){
+    generateToken(res,user._id);
+    res.json({
+      _id:user._id,
+      name: user.name,
+      email: user.email,
+    });
+  }else{
+    res.status(401);
+    throw new Error('Invalid email or password');
   }
 });
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h2>Mern app</h2>
+    </div>
+  )
+}
+
+export default App
